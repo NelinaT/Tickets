9a767e908d482e08fa5063a7ad8e950d98030e66
kkunov <kkunov121@gmail.com>
2023-02-26 18:08:50 +0200

    Fix the problem with not loading CSS

--- app.js
+++ app.js
@@ -1,13 +1,22 @@
 const express = require("express");
 const app = express();
-
+var path = require('path');
 
 app.set("view engine", "pug")
 
-app.get("/", (req,res)=>{
+app.get("/", (req,res) => {
     res.render("index")
 });
 
-
+app.get('/css/*.css', function(req, res) {
+    var fileName = '.' + req.originalUrl.substring(0, (req.originalUrl.includes("?") ? req.originalUrl.indexOf("?") : undefined));
+    // Double check that we're working in the project folder
+    if (fileName.startsWith(".")) {
+        res.sendFile(path.resolve(fileName));
+    }
+    else {
+        res.sendStatus(500);
+    }
+});
 
 app.listen(3000);
