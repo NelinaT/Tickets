376c6e0314d49b6cd04458c564cb15cd9af5b722
Nelina Toteva <nelinatoteva@gmail.com>
2023-02-28 20:09:39 +0200

    css added

--- app.js
+++ app.js
@@ -29,5 +29,20 @@ app.get('/js/*.js', function(req, res) {
         res.sendStatus(500);
     }
 });
+app.get('/images/*', function(req, res) {
+    var fileName = '.' + req.originalUrl.substring(0, (req.originalUrl.includes("?") ? req.originalUrl.indexOf("?") : undefined));
+    // Double check that we're working in the project folder
+    if (fileName.startsWith(".") && 
+        (fileName.endsWith('.jpeg') || 
+         fileName.endsWith('.png') || 
+         fileName.endsWith('.jpg') || 
+         fileName.endsWith('.svg') || 
+         fileName.endsWith('.gif'))) {
+        res.sendFile(path.resolve(fileName));
+    }
+    else {
+        res.sendStatus(403);
+    }
+});
 
 app.listen(3000);
